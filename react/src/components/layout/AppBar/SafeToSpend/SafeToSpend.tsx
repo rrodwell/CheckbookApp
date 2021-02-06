0b5dd835d65ffac70168ee0ddc44f60e602893d2
Ryan Rodwell <ryanrodwell@gmail.com>
2021-02-06 13:11:35 -0600

    building out balance and safe to spend accordion

--- react/src/components/layout/AppBar/SafeToSpend/SafeToSpend.tsx
+++ react/src/components/layout/AppBar/SafeToSpend/SafeToSpend.tsx
@@ -1,21 +1,90 @@
 import React from 'react'
-import { Paper, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+import { Grid, Typography } from '@material-ui/core'
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import ArrowDropDownCircleOutlinedIcon from '@material-ui/icons/ArrowDropDownCircleOutlined'
+import numeral from 'numeral'
 
-const amounts = ['available balance', 'in scheduled activities', 'in Expenses', 'in Goals']
+const amounts = {
+  balance: {
+    value: 1123.35,
+    text: 'available balance'
+  },
+  scheduled: {
+    value: 0,
+    text: 'in scheduled activities'
+  },
+  goals: {
+    value: 345.38,
+    text: 'in Goals'
+  },
+  expenses: {
+    value: 457.25,
+    text: 'in Expenses'
+  }
+}
+
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: '1rem',
+    backgroundColor: '#f5f5f5'
+  },
+  expanded: {
+    margin: '0 !important'
+  },
+  summary: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 0,
+    flexGrow: 0
+  }
+}))
 
 export default function SafeToSpend() {
+  const classes = useStyles()
+  const { balance, scheduled, expenses, goals } = amounts
+  const remaining = balance.value - scheduled.value - expenses.value - goals.value
+
+  numeral.defaultFormat('$0.00')
+
   return (
-    <Paper square className='full-flex' style={{ padding: '1rem' }}>
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          Spend Amount
-        </Grid>
-        {amounts.map(amount => (
-          <Grid item xs={3}>
-            {amount}
+    <Accordion className={`full-flex ${classes.root}`} classes={{ expanded: classes.expanded }} defaultExpanded>
+      <AccordionSummary
+        id='balance-header'
+        classes={{ content: classes.summary }}
+        expandIcon={<ArrowDropDownCircleOutlinedIcon color='primary' />}
+        aria-controls='balance-content'
+      >
+        <Typography variant='h4' display='inline'>
+          {numeral(remaining).format()}
+        </Typography>
+        <Typography variant='h5' display='inline' color='textSecondary' style={{ padding: '0 0.5rem' }}>
+          Remaining
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {Object.values(amounts).map((amount, idx) => (
+          <Grid
+            key={amount.text}
+            item
+            xs={3}
+            zeroMinWidth
+            style={{
+              textAlign: 'center',
+              borderRight: idx < 3 ? '1px solid' : 'none'
+            }}
+          >
+            <Typography variant='h6' display='inline'>
+              {numeral(amount.value).format()}
+            </Typography>
+            <Typography variant='subtitle1' display='inline' color='textSecondary' style={{ padding: '0 0.5rem' }}>
+              {amount.text}
+            </Typography>
           </Grid>
         ))}
-      </Grid>
-    </Paper>
+      </AccordionDetails>
+    </Accordion>
   )
 }
